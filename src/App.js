173d66f9b6b94baec94f09e8dbfa8b03c032116f
Wijaya <wijayaac25@gmail.com>
2020-11-22 14:16:16 +0800

    reusable component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Header from './component/Header';
+
 
 class App extends Component {
+  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
   }
   render() {
     return (
       <div>
-        <h1>Kadek Wijaya</h1>
+        <Header title="Hello Wijaya" subTitle="Ini buatan wijaya" />
       </div>
     );
   }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,6 @@
 import React, { Component } from 'react';
 
-export class Header extends Component {
-    // eslint-disable-next-line no-useless-constructor
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
-}
-
-export class HeaderWithSub extends Component {
+class Header extends Component {
     // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
@@ -29,3 +15,5 @@ export class HeaderWithSub extends Component {
         );
     }
 }
+
+export default Header;
