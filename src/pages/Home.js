d50eafe9eba6cfe2c4ca82c99c2df77b03618448
Wijaya <wijayaac25@gmail.com>
2020-11-24 16:02:15 +0800

    consume api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
-export default function Home() {
-    return (
-        <div>
-            <h1>Home</h1>
-            <p>Welcome to home</p>
-        </div>
-    );
+
+
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: []
+        }
+    }
+    componentDidMount() {
+        this._getData();
+    }
+    async _getData() {
+        const options = {
+            headers: { 'Content-Type': 'application/json' },
+            mehod: 'GET'
+        };
+        try {
+            const data = await fetch('https://jsonplaceholder.typicode.com/users', options);
+            const jsonData = await data.json();
+            this.setState({ items: jsonData });
+        } catch (e) {
+            console.log(e);
+        }
+
+    }
+    render() {
+        const { items } = this.state;
+        return (
+            <div>
+                <h1>User Home</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map(data => {
+                            return (
+                                <tr key={data.id} >
+                                    <td>{data.id} </td>
+                                    <td>{data.name} </td>
+                                    <td>{data.email} </td>
+                                    <td>{data.phone} </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+            </div>
+        );
+    }
 }
