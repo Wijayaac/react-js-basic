674e93cce9f03b4e54964f009bb94d4e515b2803
Wijaya <wijayaac25@gmail.com>
2020-11-24 14:58:59 +0800

    react-router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import Header from './component/Header';
-import Login from './component/Login';
-
+import { Form, Navbar, Button, Nav, FormControl } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 class App extends Component {
   // eslint-disable-next-line no-useless-constructor
@@ -11,8 +13,37 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header title="Hello Wijaya" subTitle="Ini buatan wijaya" />
-        <Login />
+        <Router>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand >Navbar</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link >
+                <Link to="/" >
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link >
+                <Link to="/register" >
+                  Register
+                </Link>
+              </Nav.Link>
+              <Nav.Link >
+                <Link to="/login" >
+                  Login
+                </Link>
+              </Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Navbar>
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route exact path="/register"><Register /></Route>
+            <Route exact path="/login"><Login /></Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
